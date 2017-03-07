af8bf84c2c1a7bfaf5cd2f75b8891e576c768e60
Gabe Dimas <gabe.dimas10@gmail.com>
2017-03-07 09:37:00 +0700

    1301154245 | 1301154413 | 1301150021 - Tugas Besar PBO IF 39-07

--- Aplikasi Pemesanan Transportasi Online/src/aplikasi_pemesanan_transportasi_online/Main.java
+++ Aplikasi Pemesanan Transportasi Online/src/aplikasi_pemesanan_transportasi_online/Main.java
@@ -3,7 +3,7 @@ package aplikasi_pemesanan_transportasi_online;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
--- Aplikasi Pemesanan Transportasi Online/src/aplikasi_pemesanan_transportasi_online/Orang.java
+++ Aplikasi Pemesanan Transportasi Online/src/aplikasi_pemesanan_transportasi_online/Orang.java
@@ -1,5 +1,36 @@
 package aplikasi_pemesanan_transportasi_online;
 
 public class Orang {
+    protected String nama, email, nomorTelepon;
+
+    public Orang(String nama, String email, String nomorTelepon) {
+        this.nama = nama;
+        this.email = email;
+        this.nomorTelepon = nomorTelepon;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public void setNomorTelepon(String nomorTelepon) {
+        this.nomorTelepon = nomorTelepon;
+    }
     
 }
