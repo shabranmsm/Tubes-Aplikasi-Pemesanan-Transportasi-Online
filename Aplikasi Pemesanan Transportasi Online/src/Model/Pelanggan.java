f21f61e264dd791dfc4b6c0c80b17d0b03c97637
Christianjanifer <christiansimanjuntak12@gmail.com>
2017-04-08 19:04:42 +0700

    1301150021_Hendrik Christian Done

--- Aplikasi Pemesanan Transportasi Online/src/Model/Pelanggan.java
+++ Aplikasi Pemesanan Transportasi Online/src/Model/Pelanggan.java
@@ -9,16 +9,57 @@ public class Pelanggan extends Orang{
     private int idx;
 
     //Constructor here
+
+    public Pelanggan(String idCustomer, String email, String nama, String noTelp, String password, String username) {
+        super(nama, noTelp, password, username);
+        this.idCustomer = idCustomer;
+        this.email = email;
+    }
+    
     
     public void createPesanan(Pesanan P) {
-
+        daftarPesanan.add(P);
     }
 
     public void removePesanan(int idx) {
-        
+        for(Pesanan P : daftarPesanan){
+            if(P.getIdPesanan().equals(idx)){
+                daftarPesanan.remove(P);
+            }
+        }
     }
 
     //List here
-
+    
+     public List<Pesanan> getDaftarPesanan() {
+        return daftarPesanan;
+    }
+     
     //Getter here
+
+    public String getIdCustomer() {
+        return idCustomer;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
+
+    @Override
+    public String toString() {
+        return  "Username : \n" + getUsername()
+                + "Password : \n" + getPassword()
+                + "IdCustomer : \n" + idCustomer 
+                + "Nama : \n" + getNama() 
+                + "Email = \n" + email 
+                + "NoTelp : \n" + getNoTelp();
+    }
 }
